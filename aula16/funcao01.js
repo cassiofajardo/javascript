3991ba0f49f655664df8a445df0d6bc61a98f8ab
Cássio Fajardo Machado <107512688+cassiofajardo@users.noreply.github.com>
2022-07-27 16:30:16 -0300

    exercícios

---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    }else {
+        return 'impar'
+    }
+}
+
+console.log(parimpar(223)) //"parimpar" é a chamada e "223" o parâmetro
