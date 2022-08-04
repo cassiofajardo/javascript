755620a7e5146ec2fc5ca0db6a179515ef9964c1
Cássio Fajardo Machado <107512688+cassiofajardo@users.noreply.github.com>
2022-08-04 15:08:08 -0300

    curso básico de JS concluído

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -1,14 +1,64 @@
-let num = document.getElementById('txtn')
-let tab = document.getElementById('seltab')
-function adicionar() {
-    let n = Number(num.value)
-    if (n < 1 || n > 100) {
-        window.alert('Valor inválido ou já encontrado na lista')
-    } else { 
-        let item = document.createElement('option')
-        item.text = n
-        tab.appendChild(item) // tabuada "adicionar elemento filho" item
-    }
-    
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+var array = []
+
+function isNumber(n) {
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else {
+        return false
+    } 
 }
-var array = [n]
+
+function inList(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }   
+}
+
+function adicionar() {
+    if(isNumber(num.value) && !inList(num.value, array)) {
+        array.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+        
+    } else{
+        window.alert('Valor inválido ou já encontrado na lista')
+    }    
+    num.value = ''
+    num.focus() 
+}  
+
+    function finalizar() {
+        if (array.length == 0) {
+            window.alert('Adicione valores para analisar.')
+        } else {
+            let total = array.length
+            let maior = array[0]
+            let menor = array[0]
+            let soma = 0
+            let media = 0
+            for(let pos in array) {
+                soma += array[pos]
+                if(array[pos] > maior)
+                maior = array[pos]
+                if(array[pos] < menor)
+                menor = array[pos]
+            }
+            media = soma / total 
+            res.innerHTML = ''
+            res.innerHTML += `<p> Temos ${total} números cadastrados.</p>`
+            res.innerHTML += `<p> O maior valor informado é: ${maior}</p>`
+            res.innerHTML += `<p> O menor valor informado é: ${menor}</p>`
+            res.innerHTML += `<p> A soma dos valores informados é: ${soma}</p>`
+            res.innerHTML += `<p>A média dos valores informados é: ${media}</p>`
+        }
+        
+    }
+
+
